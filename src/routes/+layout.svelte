76ef27ccd2780050df028f92c0742b98aa87e016
bwhdx <benedictdixon@hotmail.co.uk>
2024-12-18 14:27:04 -0700

    Updated meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,31 +4,7 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Chakra+Petch:wght@300;400;500;600;700&display=swap"
-		rel="stylesheet"
-	/>
 	<base href="{base}/" />
-
-	<!-- Basic SEO -->
-	<title>Voi Ecosystem</title>
-	<meta name="description" content="Voi is a revolutionary L1 blockchain designed to fulfill the promises of Web3. A highly performant network truly owned, run, and built by its community." />
-	
-	<!-- OpenGraph / Facebook -->
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://ecosystem.voi.network" />
-	<meta property="og:title" content="Voi Ecosystem - Built by You. Run by You. Owned by You." />
-	<meta property="og:description" content="Voi is a revolutionary L1 blockchain designed to fulfill the promises of Web3. A highly performant network truly owned, run, and built by its community." />
-	<meta property="og:image" content="https://ecosystem.voi.network/social_preview.png" />
-
-	<!-- Twitter -->
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:url" content="https://ecosystem.voi.network" />
-	<meta name="twitter:title" content="Voi Ecosystem - Built by You. Run by You. Owned by You." />
-	<meta name="twitter:description" content="Voi is a revolutionary L1 blockchain designed to fulfill the promises of Web3. A highly performant network truly owned, run, and built by its community." />
-	<meta name="twitter:image" content="https://ecosystem.voi.network/social_preview.png" />
 </svelte:head>
 
 <div class="min-h-screen bg-voi-dark text-white">
